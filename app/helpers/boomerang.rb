05cd93788e17562d38159642d51522684fde3f7e
timlarcher <timlarcher@yahoo.com>
2011-08-11 07:17:08 -0700

    Remove boomerang.rb from helpers only needed for command line version.

--- app/helpers/boomerang.rb
+++
@@ -1,7 +0,0 @@
-require_relative '../lib/boomerang/db_connection'
-require_relative '../lib/boomerang/client'
-require_relative '../lib/boomerang/matcher'
-
-Boomerang::Client.find(:all).each do |client|
-  Boomerang::Matcher::do_match( client.id )
-end
